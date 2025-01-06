0affd2b2a01964e7a6138f1d7552fd8c4aa39f05
Thorinori <thorinori@gmail.com>
2025-01-05 18:13:46 -0700

    Changed script runner for rofi
    Changed from script to different method that follows path

--- scripts/rofi_script_runner.zsh
+++ scripts/rofi_script_runner.zsh
@@ -1,3 +1,4 @@
 #!/bin/zsh
+
 rofi -e "$(zsh -c "$(rofi -config ~/.config/rofi/rofidmenu.rasi -dmenu -p 'Run command' -theme-str 'listview {lines: 0;}')" 2>&1 )" -config ~/.config/rofi/rofidmenu.rasi
 #From https://www.reddit.com/r/i3wm/comments/lhilro/execute_a_command_from_rofi/
